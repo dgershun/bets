0933e488979650f0963e58336ac57c93ddd202e6
Dmitry Gershun <dmitriy.gershun@gmail.com>
2020-08-09 14:19:46 +0300

    Add filter by 19 oclock for current season games

--- bot-main/livescore-service/index.js
+++ bot-main/livescore-service/index.js
@@ -7,24 +7,34 @@ class LivescoreService {
     }
 
     async getCLComingGames() {
-        return (await this._client
-            .coming({ competitionId: CL_COMPETITION_ID }))
-            .map((params) => new ComingGame(params));
+        return (
+            await this._client.coming({
+                competitionId: CL_COMPETITION_ID,
+            })
+        )
+        // tmp for season 2020
+        .filter(({time}) => time.split(':')[0] === '19')
+        .map((params) => new ComingGame(params));
     }
 
     async getCLPastGames() {
         return (await this._client
             .past({ competitionId: CL_COMPETITION_ID, from: '2020-01-01' }))
-            .map((params) => new PastGame(params));
+        .map((params) => new PastGame(params));
     }
 
     async getCLLiveGames() {
-        return (await this._client
-            .live({ competitionId: CL_COMPETITION_ID }))
-            .map((params) => new LiveGame(params));
+        return (
+            await this._client.live({
+                competitionId: CL_COMPETITION_ID,
+            })
+        )
+        // tmp for season 2020
+        .filter(({time}) => time.split(':')[0] === '19')
+        .map((params) => new LiveGame(params));
     }
 }
 
 module.exports = {
-    LivescoreService
+    LivescoreService,
 };
